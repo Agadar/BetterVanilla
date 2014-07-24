9e942aa5399c35a1ffb923a13c73f627904b117d
Agadar <martin_donkersloot@hotmail.com>
2014-07-24 08:30:07 +0200

    Minor change.

--- java/com/agadar/bettervanilla/blocks/BlockFluidMilk.java
+++ java/com/agadar/bettervanilla/blocks/BlockFluidMilk.java
@@ -6,6 +6,10 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -52,4 +56,18 @@ public class BlockFluidMilk extends BlockFluidClassic
 		if (world.getBlock(x, y, z).getMaterial().isLiquid()) return false;
 		return super.displaceIfPossible(world, x, y, z);
 	}    
+	
+	@Override
+    public void onEntityCollidedWithBlock(World p_149670_1_, int p_149670_2_, int p_149670_3_, int p_149670_4_, Entity p_149670_5_) 
+	{
+		if (!(p_149670_5_ instanceof EntityPlayer))
+    	{
+    		return;
+    	}
+    	
+        if (!p_149670_1_.isRemote)
+        {
+        	((EntityPlayer) p_149670_5_).curePotionEffects(new ItemStack(Items.milk_bucket));
+        }
+	}
 }
